6521f190a0eba0b702c7875a440d4624d3cb7ebf
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2019-03-10 17:49:19 +0100

    🔌 Compatibility with Python 3.6 asyncio library.

---
+++ genlib/__init__.py
@@ -0,0 +1,11 @@
+# genlib — Copyright (c) 2019, Alex J. Champandard. Code licensed under the GNU AGPLv3.
+
+import asyncio
+
+# Backwards compatibility with Python 3.6 for `current_task`.
+if not hasattr(asyncio, "current_task"):
+    asyncio.current_task = lambda: asyncio.Task.current_task()
+
+# Backwards compatibility with Python 3.6 for `create_task`.
+if not hasattr(asyncio, "create_task"):
+    asyncio.create_task = lambda t: asyncio.get_event_loop().create_task(t)
