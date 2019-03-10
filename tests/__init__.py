f74e042adde4379fb7bb408b2627785886e37c5c
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2019-03-10 17:26:11 +0100

    🛠️ Enabled Python 3.6 for builds, correct error codes returned.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,10 @@
 # genlib — Copyright (c) 2019, Alex J. Champandard. Code licensed under the GNU AGPLv3.
 
+import sys
 import pytest
 
 
 def main():
     """Entry point for Poetry run command-line with manually-specified arguments.
     """
-    pytest.main(["-c", "tests/pytest.ini"])
+    sys.exit(pytest.main(["-c", "tests/pytest.ini"]))
